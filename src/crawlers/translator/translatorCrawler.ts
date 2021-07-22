963f98eefbe066aaefea00e1708dddf984365d50
Barreto <v.barreto.and@gmail.com>
2021-07-22 20:14:11 -0300

    feat: add crawling with cluster for workers

--- src/crawlers/translator/translatorCrawler.ts
+++ src/crawlers/translator/translatorCrawler.ts
@@ -1,4 +1,6 @@
-import puppeteer, { Browser } from "puppeteer";
+import * as puppeteer from "puppeteer";
+import { Browser } from "puppeteer";
+import { Cluster } from "puppeteer-cluster";
 
 const selectors = {
   resultElement:
@@ -45,6 +47,63 @@ class TranslatorCrawler {
       return "unknow";
     } finally {
       if (this.browser) this.browser.close();
+    }
+  }
+
+  async translateMany(
+    values: string[],
+    targetLang = this.defaultLang,
+    sourceLang = "auto",
+    hideCrawler = true
+  ) {
+    let cluster: Cluster<any, any>;
+    let results = {};
+    const addResult = (value, translation) => {
+      const isKnowedValue = Object.keys(results).includes(value);
+      console.log(">>> addResult: ", { isKnowedValue, value, translation });
+
+      if (!isKnowedValue) {
+        results = { ...results, [value]: translation };
+      }
+    };
+    try {
+      cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 3,
+      });
+
+      await cluster.task(async ({ page, data: { value, addResult } }) => {
+        console.log(">>> cluster task: ", { value, addResult });
+
+        const url = `https://translate.google.com/?sl=${sourceLang}&tl=${targetLang}&op=translate&text=${value}`;
+
+        await page.goto(url);
+        const pageContext = await page.browserContext();
+        await pageContext.overridePermissions(url, ["clipboard-write", "clipboard-read"]);
+
+        await page.goto(url);
+        await page.waitForSelector(selectors.resultElement);
+        await page.waitForTimeout(this.delay);
+        await page.$eval(selectors.resultElement, (element) => element.innerHTML);
+
+        await page.focus(selectors.buttonCopyResult);
+        await page.keyboard.press("Enter");
+        const translationResult = await page.evaluate(() => navigator.clipboard.readText());
+
+        addResult(value, translationResult);
+      });
+
+      values.forEach((value) => cluster.queue({ value, addResult }));
+    } catch (error) {
+      console.error("Error: ", error);
+      return null;
+    } finally {
+      if (cluster) {
+        await cluster.idle();
+        await cluster.close();
+      }
+
+      return results;
     }
   }
 }
