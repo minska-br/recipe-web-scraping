ed9c48c09ea2e6994dbda4928ecb6561edb2166e
Barreto <v.barreto.and@gmail.com>
2021-08-01 23:31:53 -0300

    feat: include new crawler at recipe crawlers factory

--- src/crawlers/recipes/RecipeCrawlerFactory.ts
+++ src/crawlers/recipes/RecipeCrawlerFactory.ts
@@ -1,12 +1,17 @@
-import Crawlers from "../../enumerators/crawlers";
-import IRecipeCrawler from "./IRecipeCrawler";
-import TudoGostosoCrawler from "./tudoGostoso/tudoGostosoCrawler";
+import Crawlers from '../../enumerators/crawlers';
+import AllRecipesCrawler from './allRecipes/allRecipesCrawler';
+import IRecipeCrawler from './IRecipeCrawler';
+import TudoGostosoCrawler from './tudoGostoso/tudoGostosoCrawler';
 
 export default class RecipeCrawlerFactory {
   public static createRecipeCrawler(crawlerName: Crawlers): IRecipeCrawler {
     switch (crawlerName) {
       case Crawlers.TudoGostoso:
         return new TudoGostosoCrawler();
+
+      case Crawlers.AllRecipes:
+        return new AllRecipesCrawler();
+
       default:
         throw new Error(`Crawler name "${crawlerName}" not recognized.`);
     }
