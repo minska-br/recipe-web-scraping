c6027830b7a8273d33fafb1da8b925606384ff6e
Barreto <v.barreto.and@gmail.com>
2021-08-02 21:31:30 -0300

    feat: returning an recipe index in every recipe crawler get list call

--- src/services/recipesService.ts
+++ src/services/recipesService.ts
@@ -1,6 +1,7 @@
-import Recipe from '../common/Recipe';
+import RecipeIndexItem from '../common/interfaces/RecipeIndexItem';
+import Recipe from '../common/models/Recipe';
 import { error, info } from '../config/Logger';
-import RecipeCrawlerFactory from '../crawlers/recipes/RecipeCrawlerFactory';
+import RecipeCrawlerFactory from '../crawlers/recipes/factory/RecipeCrawlerFactory';
 import Crawlers from '../enumerators/crawlers';
 import LanguageCode from '../enumerators/language-codes';
 import NAMESPACES from '../enumerators/namespaces';
@@ -9,13 +10,30 @@ import TranslatiosService from './translationsService';
 class RecipesService {
   constructor(private translatiosService = new TranslatiosService()) {}
 
-  async list(crawlerName: Crawlers, value = "test") {
+  async list(
+    crawlerName: Crawlers,
+    value = "test",
+    sourceLang = LanguageCode.pt
+  ): Promise<RecipeIndexItem[] | null> {
+    const infoObj = { crawlerName, value, sourceLang };
+    info(NAMESPACES.RecipesService, "list", infoObj);
+
     const recipeCrawler = RecipeCrawlerFactory.createRecipeCrawler(crawlerName);
     const websiteLang = recipeCrawler.getDefaultWebsiteLanguage();
+    const isTranslationDependent = sourceLang !== websiteLang;
 
     try {
       const valueToSearch = await this.translatiosService.translate(value, websiteLang);
-      return await recipeCrawler.getList(valueToSearch ?? value);
+      const recipeIndex = await recipeCrawler.getList(valueToSearch ?? value);
+
+      if (recipeIndex && isTranslationDependent) {
+        const translatedRecipeIndex = await this.translatiosService.translateRecipeIndex(
+          recipeIndex,
+          sourceLang
+        );
+        return translatedRecipeIndex || recipeIndex;
+      }
+      return recipeIndex;
     } catch (err) {
       error(NAMESPACES.RecipesService, "list", err);
       return null;
@@ -27,6 +45,9 @@ class RecipesService {
     value = "test",
     targetLang = LanguageCode.en
   ): Promise<Recipe | null> {
+    const infoObj = { crawlerName, value, targetLang };
+    info(NAMESPACES.RecipesService, "searchFirst", infoObj);
+
     let translatedRecipe: Recipe | null;
     const recipeCrawler = RecipeCrawlerFactory.createRecipeCrawler(crawlerName);
     const websiteLang = recipeCrawler.getDefaultWebsiteLanguage();
@@ -57,9 +78,10 @@ class RecipesService {
     id: number,
     targetLang = LanguageCode.en
   ): Promise<Recipe | null> {
-    const recipeCrawler = RecipeCrawlerFactory.createRecipeCrawler(crawlerName);
-    const infoObj = { id, targetLang, recipeCrawler };
+    const infoObj = { id, targetLang, crawlerName };
     info(NAMESPACES.RecipesService, "searchById - recipeCrawler", infoObj);
+
+    const recipeCrawler = RecipeCrawlerFactory.createRecipeCrawler(crawlerName);
 
     try {
       const crawledRecipe = await recipeCrawler.getDetailById(id);
